5d54046f7aef51b914dd9a9f3f1abdaa0a9c0174
mulhern <amulhern@redhat.com>
2015-07-23 17:12:25 -0400

    Make Entry accessible from top level.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- storage_alerts/sources/journal/__init__.py
+++ storage_alerts/sources/journal/__init__.py
@@ -1,5 +1,6 @@
 """ Module that implements scanning of journal entries. """
 
+from .entry import Entry
 from .recognizer import Recognizer
 from .recognizer import RecognizerStates
 from .scanner import Scanner
